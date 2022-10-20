cf479389ac6f5ab4180f6cb130f96bbbe4b6d316
xartv <xvasartx@gmail.com>
2022-10-20 19:42:21 +0400

    add basic service-class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
+
+const newMarvel = new MarvelService();
+
+newMarvel.getAllCharacters()
+	.then(res => res.data.results.forEach(item => console.log(item.name) ));
+
+newMarvel.getCharacter(1011196)
+	.then(res => console.log(`Single character - ${res.data.results[0].name}`));
+	
 
 ReactDOM.render(
   <React.StrictMode>
